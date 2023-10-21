c2b8758cb75ad3522888be286900753d65b2ab34
andiargetlam <andiargetlam@gmail.com>
2023-10-21 18:49:15 +0700

    Initial Commit
    First Commit (only basic header, footer and homepage code)

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!-- Footer -->
+<footer class="footer bg-dark text-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; <?php echo date("Y"); ?> Your Company Name. All rights reserved.</p>
+            </div>
+            <div class="col-md-6 text-right">
+                <a href="mailto:youremail@example.com" class="text-light">Email Us</a>
+                <a href="https://wa.me/your-whatsapp-number" class="text-light ml-3">WhatsApp</a>
+                <a href="https://www.facebook.com/your-facebook" class="text-light ml-3"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.instagram.com/your-instagram" class="text-light ml-3"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Include Bootstrap and other JavaScript files at the end of the body if needed -->
+<script src="public/js/bootstrap.min.js"></script>
+<!-- Your custom JavaScript can be included here -->
